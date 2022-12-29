97392a5e649c365d202c5346b917e0b166071f8a
HannyCarballo <vce11662@innovaccion.mx>
2022-12-29 14:43:17 -0600

    Método constructor

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # Creando una clase
 class User:
     nombre = "Uriel"
+
+    def __init__(self):
+        # Método constructor
+
     def saludar(self, saludo):
         print(saludo+self.nombre)
 
