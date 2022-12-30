c85d983cba46e14950f2024632a051dc7e5fd8b7
HannyCarballo <vce11662@innovaccion.mx>
2022-12-30 16:41:04 -0600

    Abstraccion

--- main2.py
+++ main2.py
@@ -3,14 +3,25 @@ class FilaBanco:
 
     def siguiente_usuario(self,numero):
         # Implementacion
-        return usuarios(numero)
+        return self.obtener(numero)
 
     def formar_usuario(self,numero,usuario):
-        usuarios[ticket] = usuario
+        self.usuarios.agregar(numero,usuario)
 
 class Hash():
     data = {}
+ 
     def obtener(self,key):
         data[key]
+ 
     def agregar(self,key,value):
         datos[llave]=valor
+
+class Queue:
+    data = []
+ 
+    def obtener(self):
+        data[0]
+ 
+    def agregar(self,key,value):
+        datos[len(datos)-1]=valor
