0c89156092577854e317887f61f90c1db9439a6e
HannyCarballo <vce11662@innovaccion.mx>
2022-12-30 16:15:57 -0600

    Otra forma de asignar métodos accesores

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 # Creando una clase
 class User:
+    __edad = 0
     def __init__(self, nombre):
         # Método constructor
         self._nombre=nombre
         
     def saludar(self, saludo):
         print(saludo+self.nombre)
+
+    @property
+    def edad(self): #getter
+        return self.__edad
+
+    @edad.setter
+    def edad(self.valor):
+        if(valor<0):
+            raise ValueError("Edad no puede ser menor que 0")
+        self.__edad = valor
 
 class Empleado(User):
     #propiedades unicamente de esta clase
@@ -20,3 +31,7 @@ class Empleado(User):
     def saludar(self):
         super().saludar("Hola!")
         print("Mi nombre es "+self._nombre+" y gano: "+str(self.salario))
+
+empleado = Empleado("Uriel")
+empleado.edad = 26
+print(empleado.edad)
