6914dc0055d006f5046c15c0c0b4d83eaf3295c2
HannyCarballo <vce11662@innovaccion.mx>
2022-12-30 16:32:33 -0600

    Código no lo suficientemente abstracto

---
+++ main2.py
@@ -0,0 +1,9 @@
+class FilaBanco:
+    usuarios={}
+
+    def siguiente_usuario(self,numero):
+        # Implementacion
+        return usuarios(numero)
+
+    def formar_usuario(self,numero,usuario):
+        usuarios[ticket] = usuario
