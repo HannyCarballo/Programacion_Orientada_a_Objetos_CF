73d8c0762c8bd409f02ccef316ab5e286eb9e364
HannyCarballo <vce11662@innovaccion.mx>
2022-12-30 14:12:38 -0600

    Metodos unicos de empleado

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # Creando una clase
 class User:
-
     def __init__(self, nombre):
         # Método constructor
         self.nombre=nombre
@@ -9,7 +8,16 @@ class User:
         print(saludo+self.nombre)
 
 class Empleado(User):
-    pass
+    #propiedades unicamente de esta clase
+    salario = 0
+
+    def modificar_salario(self,salario):
+        self.salario = salario
+
+    def ver_salario(self):
+        print(self.salario)
 
 empleado = Empleado("Hannita :)")
 empleado.saludar("Hola! me llamo ")
+empleado.modificar_salario(1000)
+empleado.ver_salario()
