db40c07b8dd2c7c467be0d836815c6e3a5fef766
HannyCarballo <vce11662@innovaccion.mx>
2022-12-30 16:03:47 -0600

    Propiedad privada

--- main.py
+++ main.py
@@ -9,17 +9,18 @@ class User:
 
 class Empleado(User):
     #propiedades unicamente de esta clase
-    salario = 0
+    __salario = 0
 
     def modificar_salario(self,salario):
-        self.salario = salario
+        self.__salario = salario
 
     def ver_salario(self):
-        print(self.salario)
+        print(self.__salario)
 
     def saludar(self):
         super().saludar("Hola!")
         print("Mi nombre es "+self._nombre+" y gano: "+str(self.salario))
 
 empleado = Empleado("Hannita :)")
-print(empleado._nombre)
+# se pueden modificar una propiedad privada
+print(empleado.__salario)
